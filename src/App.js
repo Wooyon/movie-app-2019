552896ae887ecd86764333285cda768e3acd593b
Wooyon <48887513+Wooyon@users.noreply.github.com>
2019-11-17 17:28:25 +0900

    #2.2 what is map in js and how to use it

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React from "react";
 
-function Food({ favorite }) {
+function Food({ name, picture }) {
   // props
-  console.log(favorite); //props.fav도 다른 방법이다.
-  return <h1>I love {favorite}</h1>; //아래 Food component에서 정한 prop를 Food함수에서 가져와 return할 때 {fav}를 통해 사용가능.
+
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "kimchi",
+    image:
+      "https://t1.daumcdn.net/liveboard/dailylife/222d88e5c7dc496c8e8a8a56c3452e52.JPG"
+  },
+  {
+    name: "ramen",
+    image:
+      "https://t1.daumcdn.net/liveboard/dailylife/222d88e5c7dc496c8e8a8a56c3452e52.JPG"
+  },
+  {
+    name: "donkas",
+    image:
+      "https://t1.daumcdn.net/liveboard/dailylife/222d88e5c7dc496c8e8a8a56c3452e52.JPG"
+  },
+  {
+    name: "kimbab",
+    image:
+      "https://t1.daumcdn.net/liveboard/dailylife/222d88e5c7dc496c8e8a8a56c3452e52.JPG"
+  }
+];
 
 function App() {
   return (
     <div>
       <h1>Hello!!!</h1>
-      {/* Food함수를 총네번 각각 호출한다 */}
-      <Food favorite="kimchi" />
-      <Food favorite="ramen" />
-      <Food favorite="noodle" />
-      <Food favorite="pizza" />
+      {/* dish는 foodILkie배열의 각각item이고 => 까지의 해석은 dish라는 각각의 아이템에 함수를 적용해서 사용해준다 라는 의미.*/}
+      {foodILike.map(dish => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
