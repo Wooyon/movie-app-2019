aa16fa70b9d1d43b328a13c0a8d26679aa29b560
Wooyon <48887513+Wooyon@users.noreply.github.com>
2019-11-17 16:29:53 +0900

    #2.2 jsx + props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({ favorite }) {
+  // props
+  console.log(favorite); //props.fav도 다른 방법이다.
+  return <h1>I love {favorite}</h1>; //아래 Food component에서 정한 prop를 Food함수에서 가져와 return할 때 {fav}를 통해 사용가능.
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!!!</h1>
+      {/* Food함수를 총네번 각각 호출한다 */}
+      <Food favorite="kimchi" />
+      <Food favorite="ramen" />
+      <Food favorite="noodle" />
+      <Food favorite="pizza" />
     </div>
   );
 }
